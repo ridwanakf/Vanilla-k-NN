749bbb7d21564c4a98013ebd48d73fb25f28bfcd
ridwanakf <blog.afwan@gmail.com>
2019-05-25 19:37:51 +0700

    Added capability for k > 1 and fix some errors

--- VanillaKNN.py
+++ VanillaKNN.py
@@ -1,7 +1,13 @@
+# Created by Ridwan Afwan Karim Fauzi
+# https://www.github.com/ridwanakf
+
 class VanillaKNN():
-    def fit(self, X_train, y_train):
+    def fit(self, X_train, y_train, num_of_k=1):
         self.X_train = X_train
         self.y_train = y_train
+        self.num_of_k = num_of_k
+        self.k_list_x = []
+        self.k_list_y = []
 
     def predict(self, X_test):
         predictions = []
@@ -11,16 +17,38 @@ class VanillaKNN():
         return predictions
 
     def closest(self, row):
-        best_dist = euclidean_distance(row, self.X_train[0])
-        best_index = 0
+        self.k_list_x.clear()
+        self.k_list_x.append(self.euclidean_distance(row, self.X_train[0]))
+
+        self.k_list_y.clear()
+        self.k_list_y.append(0)
 
         for i in range(1, len(self.X_train)):
-            if euclidean_distance(row, self.X_train[i]) < best_dist:
-                best_dist = euclidean_distance(row, self.X_train[i])
-                best_index = i
-        return self.y_train[best_index]
+            dist_now = self.euclidean_distance(row, self.X_train[i])
+            if len(self.k_list_x) < self.num_of_k:
+                self.k_list_x.append(dist_now)
+                self.k_list_y.append(i)
+            elif dist_now < max(self.k_list_x):
+                index_max = self.k_list_x.index(max(self.k_list_x))
+                self.k_list_x[index_max] = dist_now
+                self.k_list_y[index_max] = i
 
-    def euclidean_distance(a, b):
+        # get all classes of nearest neighbors
+        y_predict = [y_train[self.k_list_y[i]]
+                     for i in range(len(self.k_list_y))]
+
+        # count for the most appeared class
+        predicted = list(max(zip((y_predict.count(item)
+                                  for item in set(y_predict)), set(y_predict))))
+
+        # if all of the nearest classes are unique, get the closest one
+        if len(set(y_predict)) == self.num_of_k:
+            index_min = self.k_list_x.index(min(self.k_list_x))
+            return y_train[self.k_list_y[index_min]]
+        else: # else got the most appeared class
+            return predicted[1]
+
+    def euclidean_distance(self, a, b):
         if len(a) != len(b):
             print('Vectors are not in the same length!')
             return 0
